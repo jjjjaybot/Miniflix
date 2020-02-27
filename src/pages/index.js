515e3ac3bfd7216a810eed3f3ac53a9245d78e19
Jiazi Cai <caijiazi777@gmail.com>
2020-02-27 16:43:50 -0500

    Home page

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Header from '../components/Header';
+
+class Main extends Component {
+    state = {  }
+    render() { 
+        return ( <div>
+            <Header />
+        </div> );
+    }
+}
+ 
+export default Main;
